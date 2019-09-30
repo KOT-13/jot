7a914a97e3dff73451f1576be599a1833cf8d8f6
alexn <anazarenco@best4u.md>
2019-09-30 16:07:19 +0300

    40

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -42,9 +42,7 @@
                     <div>
                         Contacts
                     </div>
-                    <div class="rounded-full border border-gray-400 text-white bg-blue-400 w-10 h-10 flex justify-center items-center">
-                        AN
-                    </div>
+                    <UserCircle :name="user.name" />
                 </div>
 
                 <div class="flex flex-col overflow-y-hidden flex-1">
@@ -56,6 +54,7 @@
 </template>
 
 <script>
+    import UserCircle from './UserCircle';
     export default {
         name: "app",
 
@@ -63,12 +62,18 @@
             'user'
         ],
 
-        mounted() {
+        components: { UserCircle },
+
+        created() {
             window.axios.interceptors.request.use(
                 (config) => {
-                    config.data = {
-                        ...config.data,
-                        api_token: this.user.api_token
+                    if (config.method === 'get') {
+                        config.url = config.url + '?api_token=' + this.user.api_token
+                    } else {
+                        config.data = {
+                            ...config.data,
+                            api_token: this.user.api_token
+                        }
                     }
 
                     return config;
--- resources/js/components/InputField.vue
+++ resources/js/components/InputField.vue
@@ -3,9 +3,10 @@
         <label :for="name" class="text-blue-500 pt-2 uppercase text-xs font-bold absolute">{{ label }}</label>
         <input :id="name" type="text"
                class="pt-8 w-full text-gray-900 border-b pb-2 focus:outline-none focus:border-blue-400"
-               :placeholder="placeholder" v-model="value" @input="updateField(name)">
+               :class="errorClassObject()"
+               :placeholder="placeholder" v-model="value" @input="updateField()">
 
-        <p class="text-red-600 text-sm" v-text="errorMessage(name)">Error here</p>
+        <p class="text-red-600 text-sm" v-text="errorMessage()">Error here</p>
     </div>
 </template>
 
@@ -22,23 +23,35 @@
                 value: ''
             }
         },
+
+        computed: {
+            hasError: function () {
+               return this.errors && this.errors[this.name] && this.errors[this.name].length > 0;
+            }
+        },
         
         methods: {
-            updateField: function (field) {
-                this.clearErrors(field);
+            updateField: function () {
+                this.clearErrors(this.name);
 
                 this.$emit('update:field', this.value)
             },
             
-            errorMessage: function (field) {
-                if (this.errors && this.errors[field] && this.errors[field].length > 0) {
-                    return this.errors[field][0];
+            errorMessage: function () {
+                if (this.hasError) {
+                    return this.errors[this.name][0];
                 }
             },
             
-            clearErrors: function (field) {
-                if (this.errors && this.errors[field] && this.errors[field].length > 0) {
-                    return this.errors[field] = null;
+            clearErrors: function () {
+                if (this.hasError) {
+                    return this.errors[this.name] = null;
+                }
+            },
+
+            errorClassObject: function () {
+                return {
+                    'error-field': this.hasError
                 }
             }
         }
@@ -46,5 +59,7 @@
 </script>
 
 <style scoped>
-
+    .error-field {
+        @apply .border-red-500 .border-b-2
+    }
 </style>
